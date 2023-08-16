847ac64beaf62a96a5c78da02513b819fa686dfd
Islam <50731444+xislam@users.noreply.github.com>
2023-08-16 21:59:23 +0600

    True test OK

--- DriverLog/tests.py
+++ DriverLog/tests.py
@@ -4,10 +4,8 @@ from rest_framework import status
 from datetime import datetime, timedelta
 from DriverLog.models import DriverLog
 
-
 class WorkingTimeViewTest(TestCase):
     def setUp(self):
-        # Создаем тестовые данные
         self.driver_log_1 = DriverLog.objects.create(
             company_id=1,
             create_date=datetime(2023, 8, 1, 8, 0),
@@ -20,14 +18,17 @@ class WorkingTimeViewTest(TestCase):
             driver_id=123,
             status='OFF'
         )
-        # Добавьте больше тестовых данных по необходимости
 
     def test_working_time_view(self):
         start_date = '2023-08-01'
         end_date = '2023-08-07'
-        url = reverse('working-time', args=[self.driver_log_1.driver_id, start_date, end_date])
+        url = reverse('driver_log_working_time', args=[self.driver_log_1.driver_id, start_date, end_date])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['working_time'], '4 days, 4:00:00')
-        self.assertEqual(response.data['off_time'], '4 days, 0:00:00')
+
+        expected_working_time = '4:00:00'
+        expected_off_time = '0:00:00'
+
+        self.assertEqual(str(response.data['working_time']), expected_working_time)
+        self.assertEqual(str(response.data['off_time']), expected_off_time)
